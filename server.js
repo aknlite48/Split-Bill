373212a0b57457f2c256ad625b740c155943256c
aknlite48 <aanandk2705@gmail.com>
2025-03-21 21:45:44 -0400

    added background PORT to server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());
